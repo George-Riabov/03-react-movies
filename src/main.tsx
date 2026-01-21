b0b753c0eac9f2d122799b7065e2457bc45be856
George Riabov <george.riabov3105@gmail.com>
2026-01-21 11:19:05 +0100

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
-import "modern-normalize";
+import "modern-normalize/modern-normalize.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
